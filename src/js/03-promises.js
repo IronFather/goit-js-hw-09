bdc4a832c390fb9add7c5fb12470d5f76d203733
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-12-09 18:26:05 +0200

    did task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,25 +1,60 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css'
 
+const refs = {
+  delayInputEl: document.querySelector(`input[name="delay"]`),
+  stepInputEl: document.querySelector(`input[name="step"]`),
+  amountInputEl: document.querySelector(`input[name="amount"]`),
+  submitBtnEl: document.querySelector(`.form button`),
+};
 
+// refs.submitBtnEl.setAttribute(`disabled`,`true`); 
+refs.submitBtnEl.addEventListener(`click`, onCreatePromise);
 
+function onCreatePromise(e) {
+  e.preventDefault();
 
+  let firstDelay = Number(refs.delayInputEl.value);
+  let nextStepDelay = Number(refs.stepInputEl.value);
+  let amount = Number(refs.amountInputEl.value);
 
+  for (let i = 1; i <= amount; i += 1) {
+    console.log(firstDelay, nextStepDelay, amount);
 
+    createPromise(i, firstDelay).then(({position, delay}) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)})
+      .catch(({position, delay}) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)});
 
+    firstDelay += nextStepDelay;
+  };
+};
 
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+  
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        } 
+      }, delay);
+    });
+  };
 
+// Перевірка 1
+// createPromise(4, 500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+// Працює!!!
 
-
-
-
-
+// ----------------------------------------------------------------------------
 // Задание 3 - генератор промисов
 
 // В HTML есть разметка формы, в поля которой пользователь будет 
